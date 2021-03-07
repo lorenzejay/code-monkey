8ea507b5fdd3f89befff689d357b6bdecd4ef450
Lorenze Jay <Lorenzejay@gmail.com>
2021-03-07 14:10:51 -0800

    updated day 2 sign and sign up page

--- pages/signin.js
+++ pages/signin.js
@@ -1,15 +1,62 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-const Signin = () => {
+import Layout from "../components/layout";
+import PaddingWrapper from "../components/paddingWrapper";
+import { useAuth } from "../hooks/useAuth";
+
+const SignIn = () => {
+  const auth = useAuth();
+  const { user, signIn, loginWithGoogle, error } = auth;
+
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    await signIn(email, password);
+    if (error === null) {
+      router.push("/myWishList");
+    }
+  };
+
+  useEffect(() => {
+    if (user !== null) {
+      router.push("/myWishList");
+    }
+  }, [user]);
+
   return (
     <Layout>
-      <h1> Sign In </h1>
-      <form>
-        <input name="email" value="" type="email" placeholder="email" />
-        <input name="password" value="" type="password" placeholder="password" />
-      </form>
+      <PaddingWrapper className="w-full flex items-center justify-center">
+        <form
+          onSubmit={handleLogin}
+          className="flex flex-col items-center justify-center w-full lg:w-1/2"
+        >
+          <h1>Login</h1>
+          <input
+            placeholder="email"
+            type="name"
+            onChange={(e) => setEmail(e.target.value)}
+            className="border-2 border-opacity-10 border-black outline-none px-5 py-1 mt-3"
+            required
+          />
+          <input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-2 border-opacity-10 border-black outline-none px-5 py-1 mt-3"
+            required
+          />
+
+          <button type="submit" className="text-white bg-gray-800 mt-3">
+            Submit
+          </button>
+        </form>
+      </PaddingWrapper>
     </Layout>
   );
 };
 
-export default Signin;
+export default SignIn;
--- pages/signup.js
+++ pages/signup.js
@@ -1,18 +1,86 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/layout";
+import PaddingWrapper from "../components/paddingWrapper";
+import { useAuth } from "../hooks/useAuth";
 
 //form that has a username, email, passsord, confirm password
 
 const Signup = () => {
+  const auth = useAuth();
+  const { signUp, user, error } = auth;
+
+  const router = useRouter();
+  useEffect(() => {
+    if (user) {
+      return router.push("/myWishList");
+    }
+  }, [auth]);
+
+  //router
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  //form success/errors
+  const [formError, setFormError] = useState("");
+
+  const handleCreateAccount = async (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      try {
+        await signUp({ name, email, password });
+        if (error === null) {
+          router.push("/myWishList");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setFormError("Passwords do not match");
+    }
+  };
+
   return (
     <Layout>
-      <h1> Sign Up </h1>
-      <form>
-        <input name="username" value="" type="text" placeholder="username" />
-        <input name="email" value="" type="email" placeholder="email" />
-        <input name="password" value="" type="password" placeholder="password" />
-        <input name="confirm password" value="" type="password" placeholder="confirm password" />
-      </form>
+      <PaddingWrapper>
+        {error && <p className="text-red-500 text-2xl">{error.message}</p>}
+        <form className="flex flex-col w-full" onSubmit={handleCreateAccount}>
+          <h1>Sign Up</h1>
+          {formError && <p>{formError}</p>}
+          {/* {createAccountSuccess && <p>Successfully Created an account</p>}
+        {createAccountError && <p>{createAccountError}</p>} */}
+          <input
+            placeholder="name"
+            type="text"
+            className="w-full  border-2 border-opacity-20 px-5 py-1 outline-none mt-2"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            placeholder="email"
+            type="text"
+            className="w-full  border-2 border-opacity-20 px-5 py-1 outline-none mt-2"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="password"
+            type="password"
+            className="w-full border-2 border-opacity-20 px-5 py-1 outline-none mt-2"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            placeholder="confirm password"
+            type="password"
+            className="w-full border-2 border-opacity-20 px-5 py-1 outline-none mt-2"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button type="submit" className="text-white bg-gray-800 border-none rounded">
+            create account
+          </button>
+        </form>
+      </PaddingWrapper>
     </Layout>
   );
 };
