77a157e8885145fbdf65b9af615c4853ef655949
Lorenze Jay <Lorenzejay@gmail.com>
2021-03-07 14:09:49 -0800

    day 2 paddign wrapper created

--- components/paddingWrapper.js
+++ components/paddingWrapper.js
@@ -1,5 +1,5 @@
 const PaddingWrapper = ({ children, className }) => {
-  return <div className={`px-5 lg:px-16 xl:px-44  ${className}`}>{children}</div>;
+  return <div className={`sm:px-5 lg:px-8 xl:px-44  ${className}`}>{children}</div>;
 };
 
 export default PaddingWrapper;
